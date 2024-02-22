c9d41ce8ad3580cfe7efb01061b99e81d9261696
Unchpokable <Unchpokable.official@gmail.com>
2024-02-23 04:26:10 +0500

    Generators for Cones and Cylinder

--- generators.hpp
+++ generators.hpp
@@ -5,6 +5,7 @@
 #include "LineData.hpp"
 #include "SphereData.hpp"
 #include "vtkUtils.hpp"
+#include "xmltools.hpp"
 
 enum class GeometryPrimitive
 {
@@ -82,6 +83,105 @@ inline vtkActorPointer CreateSphere(const SphereData& data)
     return CreateSphere(data.Radius, data.Position, data.Color.ToEigenVector3d());
 }
 
+inline vtkActorPointer CreateCone(double radius, double height, const Eigen::Vector3d& center, const Eigen::Vector3d& color, DetailsLevel resolution = DetailsLevel::Medium)
+{
+    const auto coneSource = vtkSmartPointer<vtkConeSource>::New();
+    coneSource->SetRadius(radius);
+    coneSource->SetHeight(height);
+    coneSource->SetCenter(center[0], center[1], center[2]);
+
+    const auto res = std::pow(2, static_cast<int>(resolution));
+    coneSource->SetResolution(res);
+
+    const auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(coneSource->GetOutputPort());
+    auto actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(color[0], color[1], color[2]);
+
+    return actor;
+}
+
+inline vtkActorPointer CreateCylinder(double radius, double height, const Eigen::Vector3d& center, const Eigen::Vector3d& color, DetailsLevel resolution = DetailsLevel::Medium)
+{
+    const auto cylinderSource = vtkSmartPointer<vtkCylinderSource>::New();
+    cylinderSource->SetRadius(radius);
+    cylinderSource->SetHeight(height);
+    cylinderSource->SetCenter(center[0], center[1], center[2]);
+
+    const auto res = std::pow(2, static_cast<int>(resolution));
+    cylinderSource->SetResolution(res);
+
+    const auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputConnection(cylinderSource->GetOutputPort());
+    auto actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(color[0], color[1], color[2]);
+
+    return actor;
+}
+
+inline vtkActorPointer CreateTruncatedCone(double topRadius, double bottomRadius, double height, const Eigen::Vector3d& center, const Eigen::Vector3d& color, DetailsLevel resolution = DetailsLevel::Medium)
+{
+    // Fuck my life then
+    const int numSides = std::pow(2, static_cast<int>(resolution));
+    const auto points = vtkSmartPointer<vtkPoints>::New();
+    const auto polygons = vtkSmartPointer<vtkCellArray>::New();
+
+    // Generate a circles for top and bottom of our cone
+    for(vtkIdType i = 0; i < numSides; ++i) {
+        double angle = 2.0 * vtkMath::Pi() * i / numSides;
+        // Bottom 
+        points->InsertNextPoint(bottomRadius * cos(angle), bottomRadius * sin(angle), 0.0);
+        // top
+        points->InsertNextPoint(topRadius * cos(angle), topRadius * sin(angle), height);
+    }
+
+    // Top circle polygons
+    const auto topPolygon = vtkSmartPointer<vtkPolygon>::New();
+    topPolygon->GetPointIds()->SetNumberOfIds(numSides);
+    for(vtkIdType i = 0; i < numSides; ++i) 
+    {
+        topPolygon->GetPointIds()->SetId(i, i * 2 + 1);
+    }
+    polygons->InsertNextCell(topPolygon);
+
+    // Bottom circle polygons
+    const auto bottomPolygon = vtkSmartPointer<vtkPolygon>::New();
+    bottomPolygon->GetPointIds()->SetNumberOfIds(numSides);
+    for(vtkIdType i = 0; i < numSides; ++i) {
+        bottomPolygon->GetPointIds()->SetId(i, i * 2);
+    }
+    polygons->InsertNextCell(bottomPolygon);
+
+    // Wall
+    for(vtkIdType i = 0; i < numSides; ++i) {
+        auto wallPolygon = vtkSmartPointer<vtkPolygon>::New();
+        wallPolygon->GetPointIds()->SetNumberOfIds(4); // Rectangle polygons
+        const vtkIdType nextIndex = (i + 1) % numSides;
+        wallPolygon->GetPointIds()->SetId(0, i * 2);
+        wallPolygon->GetPointIds()->SetId(1, nextIndex * 2);
+        wallPolygon->GetPointIds()->SetId(2, nextIndex * 2 + 1);
+        wallPolygon->GetPointIds()->SetId(3, i * 2 + 1);
+
+        polygons->InsertNextCell(wallPolygon);
+    }
+
+    auto polyData = vtkSmartPointer<vtkPolyData>::New();
+    polyData->SetPoints(points);
+    polyData->SetPolys(polygons);
+
+    auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    mapper->SetInputData(polyData);
+
+    auto actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetColor(color[0], color[1], color[2]);
+    actor->SetPosition(center[0], center[1], center[2]);
+
+    return actor;
+}
+
 inline vtkActorPointer CreateLine(const Eigen::Vector3d& begin, const Eigen::Vector3d& end, const Eigen::Vector3d& color = Eigen::Vector3d(1, 1, 1))
 {
     const auto lineSource = vtkSmartPointer<vtkLineSource>::New();
@@ -126,4 +226,34 @@ inline std::vector<vtkSmartPointer<vtkActor>> CreateRandomObjectsOfType(Geometry
     }
 
     return actors;
+}
+
+
+inline Eigen::Vector3d ConvertHexToEigenVectorColor(const QString& hex)
+{
+    if(!hex.startsWith("#") || hex.length() != 7) {
+        return {};
+    }
+
+    bool ok;
+
+    const int r = hex.mid(1, 2).toInt(&ok, 16);
+    const int g = hex.mid(3, 2).toInt(&ok, 16);
+    const int b = hex.mid(5, 2).toInt(&ok, 16);
+
+    return Eigen::Vector3d(r, g, b);
+}
+
+using ProbeHeadGeometry = ProbeHeadExtensionGeometryPrimitive;
+
+inline Result<vtkActorPointer> BuildGeometryFromGeometryPrimitive(const ProbeHeadGeometry& geometry) {
+    const auto color = ConvertHexToEigenVectorColor(geometry.HexColor());
+    switch(geometry.Type()) {
+        case GeometryType::Cylinder:
+            return CreateTruncatedCone(geometry.Diameter1(), geometry.Diameter2(), geometry.Height(), { 0, 0, 0 }, color);
+        case GeometryType::Cone:
+            return CreateCone(geometry.Diameter() / 2, geometry.Height(), { 0, 0, 0 }, color);
+        case GeometryType::Sphere:
+            return CreateSphere(geometry.Diameter() / 2, { 0, 0, 0 }, color);
+    }
 }
