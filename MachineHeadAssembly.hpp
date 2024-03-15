cf05317b699f08eaf3312334da9db200b8bae7e0
Unchpokable <unchpokable.official@gmail.com>
2024-03-15 12:08:25 +0500

    The Great Refactoring Begins!

--- MachineHeadAssembly.hpp
+++ MachineHeadAssembly.hpp
@@ -11,46 +11,13 @@ class MachineHeadAssembly
     friend class MachineHead;
 
 public:
-    MachineHeadAssembly(MachinePart* part, MachineHeadAssembly* parent = nullptr) : _part(part)
-    {
-        _actor = LoadObj(_part->Models());
-        _parent = parent;
-        const auto transform = vtkSmartPointer<vtkTransform>::New();
+    MachineHeadAssembly(MachinePart* part, MachineHeadAssembly* parent = nullptr);
 
-        if (_parent != nullptr)
-        {
-            transform->PostMultiply();
-            transform->SetInput(_parent->_transform);
-        }
+    void SetPartRotation(RotAddress address, double angle) const;
 
-        _actor->SetUserTransform(transform);
+    bool HasRotation() const;
 
-        _transform = vtkTransform::SafeDownCast(_actor->GetUserTransform());
-        for(const auto child: _part->ChildMachines())
-        {
-            _childElements.push_back(new MachineHeadAssembly(child, this));
-        }
-    }
-
-    void SetPartRotation(RotAddress address, double angle) const
-    {
-        if (_part)
-            RotateImpl(address, angle);
-    }
-
-    bool HasRotation() const 
-    {
-        if (_part)
-            return _part->CanRotate();
-        return false;
-    }
-
-    RotAddress RotationAddress() const
-    {
-        if (_part)
-            return _part->RotationAxisAddress();
-        return RotAddress::None;
-    }
+    RotAddress RotationAddress() const;
 
     vtkActorPointer Actor() const
     {
@@ -62,49 +29,20 @@ public:
         return _childElements;
     }
 
-    void InsertChild(MachineHeadAssembly* child)
-    {
-        if (child)
-            _childElements.push_back(child);
-    }
+    void InsertChild(MachineHeadAssembly* child);
 
-    const MachineHeadAssembly* LastChild() const
-    {
-        if(!_childElements.empty())
-            return _childElements.back()->LastChild();
-
-        return this;
-    }
+    const MachineHeadAssembly* LastChild() const;
 
     const MachineHeadAssembly* Parent() const
     {
         return _parent;
     }
 
-    void Restore() const
-    {
-        const auto newActor = LoadObj(_part->Models());
-        _actor->SetMapper(newActor->GetMapper());
-    }
+    void Restore() const;
 
-    void UpdateChild() const
-    {
-        TransformChildren(_transform);
-    }
+    void UpdateChild() const;
 
-    void SetBaseRZ(const vtkSmartPointer<vtkTransform>& rz, bool isChild = false) 
-    {
-        _transform->DeepCopy(rz);
-        _actor->SetUserTransform(_transform);
-
-        if (!isChild)
-            _rzRotation = rz;
-
-        for (const auto child : _childElements)
-        {
-            child->SetBaseRZ(rz, true);
-        }
-    }
+    void SetBaseRZ(const vtkSmartPointer<vtkTransform>& rz, bool isChild = false);
 
     vtkSmartPointer<vtkTransform> GetRZ() const
     {
@@ -120,73 +58,11 @@ private:
     std::vector<MachineHeadAssembly*> _childElements;
     MachineHeadAssembly* _parent;
 
-    void RotateImpl(RotAddress address, double angle) const
-    {
-        if(_part->CanRotate() && _part->RotationAxisAddress() == address) 
-        {
-            const auto axis = _part->Axis().Axis();
+    void RotateImpl(RotAddress address, double angle) const;
 
-            _transform->Identity();
-            _transform->PreMultiply();
+    void TransformChildren(const vtkSmartPointer<vtkTransform>& transform) const;
 
-            if(_rzRotation && _rzRotation.GetPointer())
-                _transform->Concatenate(GetRZ());
+    void AppendMesh(const vtkActorPointer& object);
 
-            _transform->Translate(axis.X, axis.Y, axis.Z);
-            _transform->RotateWXYZ(angle, axis.I, axis.J, axis.K);
-
-            TransformChildren(_transform);
-        }
-
-        else if(!_childElements.empty()) 
-        {
-            for(const auto child : _childElements) 
-            {
-                child->RotateImpl(address, angle);
-            }
-        }
-    }
-
-    void TransformChildren(const vtkSmartPointer<vtkTransform>& transform) const
-    {
-        for(const auto child: _childElements)
-        {
-            child->_transform->PostMultiply();
-            child->_transform->Concatenate(transform);
-            child->_transform->PreMultiply();
-            child->TransformChildren(child->_transform);
-        }
-    }
-
-    void AppendMesh(const vtkActorPointer& object)
-    {
-        vtkTransform::SafeDownCast(object->GetUserTransform())->PostMultiply();
-        vtkTransform::SafeDownCast(object->GetUserTransform())->SetInput(_actor->GetUserTransform());
-        const auto newActor = MergeActors({ _actor, object }, false);
-        _actor->SetMapper(newActor->GetMapper());
-        _actor->SetUserTransform(_transform);
-    }
-
-    static vtkActorPointer LoadObj(const MachineModelList& models)
-    {
-        std::vector<vtkSmartPointer<vtkActor>> result;
-
-        for (const auto& obj: models.Parts())
-        {
-            const auto reader = vtkSmartPointer<vtkOBJReader>::New();
-            reader->SetFileName(obj.MeshFile().toStdString().c_str());
-            reader->Update();
-
-            const auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-            mapper->SetInputConnection(reader->GetOutputPort());
-
-            const auto part = vtkSmartPointer<vtkActor>::New();
-            part->SetMapper(mapper);
-            const auto color = obj.ColorF();
-            part->GetProperty()->SetColor(color.R(), color.G(), color.B());
-            result.push_back(part);
-        }
-
-        return MergeActors(result);
-    }
+    static vtkActorPointer LoadObj(const MachineModelList& models);
 };
